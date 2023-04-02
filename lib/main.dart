a2c8dd8980384f4710dd36868029fdd8947c6262
2022065432 <itsjieun@hanyang.ac.kr>
2023-04-03 00:05:04 +0900

    week4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:eos_chatting/chatting/chat/chat_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:eos_chatting/screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,7 +22,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginSignUpScreen()
+      home: ChatScreen(),
+      //home: LoginSignUpScreen()
     );
   }
 }
